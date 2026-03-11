156841eb21caf66012902a8d84eaf2db89d03767
FlorentinaDamian <damianflorentina88@gmail.com>
2026-03-11 17:11:51 +0200

    alta

--- Lab4/Clasa_fara_AI/User_profile_ui/Main.java
+++ Lab4/Clasa_fara_AI/User_profile_ui/Main.java
@@ -1,14 +1,13 @@
 public class Main {
-
     public static void main(String[] args) {
         Person user = new Person("alex123", "alex@email.com");
-
         user.setFirstName("Alex");
         user.setLastName("Popescu");
         user.setBio("Smart home enthusiast");
         user.setLocation("Bucharest");
-
+        user.setPassword("mySecret123");
         user.displayProfile();
-
+        boolean login = user.authenticate("mySecret123"); 
+        boolean fail = user.authenticate("wrongPassword");
     }
 }
